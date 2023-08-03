ef91c0383bf17a73df5ede301c7e06947317ee47
Jan <Jan-Niklas.Weder@live.de>
2023-08-03 15:16:06 +0200

    BUGFIX: Environmental Weather Recovery

--- src/PlantEd/server/environment/environment.py
+++ src/PlantEd/server/environment/environment.py
@@ -82,7 +82,7 @@ class Environment:
 
         env.water_grid = MetaboliteGrid.from_dict(dic["water_grid"])
         env.nitrate_grid = MetaboliteGrid.from_dict(dic["nitrate_grid"])
-        env.weather = WeatherSimulator.from_dict(dic["weather_states"])
+        env.weather = WeatherSimulator.from_dict(dic["weather"])
 
         return env
 
